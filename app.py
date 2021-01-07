629005c669e3d04301b45648a765903b0050682e
M-Dimbio <70532034+M-Dimbio@users.noreply.github.com>
2021-01-07 16:28:30 -0500

    reorganised the folders to match flask syntax

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -9,4 +9,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
